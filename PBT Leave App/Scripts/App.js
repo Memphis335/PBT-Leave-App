23f5df3c9c7ff3385a047c3da5a17265123a17f3
Lourens Marx <ll.marx@hotmail.com>
2015-10-23 16:58:43 +0200

    Finished Workflow

--- PBT Leave App/Scripts/App.js
+++ PBT Leave App/Scripts/App.js
@@ -11,7 +11,8 @@ var family = "";
 // This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
 $(document).ready(function () {
     getUserName();
-    $("#tblCustomListData").toggle().toggle();
+
+    window.setInterval(getLeaveRequests(username), 2000);
 });
 
 // This function prepares, loads, and then executes a SharePoint query to get the current users information
